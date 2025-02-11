2b90957f90db10930624a6f64013f81e45c62393
lgpay <7310567+lgpay@users.noreply.github.com>
2025-02-11 13:52:41 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,8 +11,8 @@ php -r "
     \$dailyCron = explode(':', \$config['Cron']['dailyjob_schedule']);
     \$dailyCron = \$dailyCron[1] . ' ' . \$dailyCron[0] . ' * * *';
     file_put_contents('$CRON_FILE', 
-        \$aliyunCron . ' php /app/aliyun-cdt-check.php > /dev/null 2>&1\n' . 
-        \$dailyCron . ' php /app/dailyjob.php > /dev/null 2>&1\n'
+        \$aliyunCron . \" php /app/aliyun-cdt-check.php > /dev/null 2>&1\n\" . 
+        \$dailyCron . \" php /app/dailyjob.php > /dev/null 2>&1\n\"
     );
 "
 
